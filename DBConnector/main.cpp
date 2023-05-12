8c2332c95cb0c80c34f8cc21bfe722b60350febd
m5623skhj <m5623skhj@gmail.com>
2023-05-13 02:19:46 +0900

    * google test 이전에 migration을 실행하도록 순서 변경

--- DBConnector/main.cpp
+++ DBConnector/main.cpp
@@ -8,6 +8,7 @@
 
 #include "ODBCConnector.h"
 #include "StoredProcedure.h"
+#include "ODBCConst.h"
 
 #include "GoogleTest.h"
 
@@ -34,17 +35,6 @@ bool TryDBMigration()
 
 int main() 
 {
-#if UNIT_TEST
-    if (GTestHelper::StartTest() == false)
-    {
-        cout << "GTest failed" << std::endl;
-        return 0;
-    }
-    cout << "---------------------" << endl;
-    cout << "GTest successed" << endl;
-    cout << "---------------------" << endl << endl << endl;
-#endif
-
     if (TryDBMigration() == false)
     {
         cout << "---------------------" << endl;
@@ -55,6 +45,17 @@ int main()
     cout << "---------------------" << endl;
     cout << "Migration successed" << endl;
     cout << "---------------------" << endl << endl << endl;
+
+#if UNIT_TEST
+    if (GTestHelper::StartTest() == false)
+    {
+        cout << "GTest failed" << std::endl;
+        return 0;
+    }
+    cout << "---------------------" << endl;
+    cout << "GTest successed" << endl;
+    cout << "---------------------" << endl << endl << endl;
+#endif
 
     ODBCConnector connector;
     do
@@ -74,6 +75,19 @@ int main()
         cout << "InitDB() Success" << endl;
     } while (false);
 
+    if (connector.CallStoredProcedure("update_test", 1) == false)
+    {
+        return 0;
+    }
+
+    auto procedure = connector.GetProcedureInfo("test");
+    procedure->SettingSPMaker(connector.GetStmtHandle(), SP_PARAMETER_LOCATION, 1, 2, 33, "testString");
+
+    if (connector.DBSendQuery(procedure->sql) == false)
+    {
+        return 0;
+    }
+    
     connector.DisconnectDB();
 
     return 0;
